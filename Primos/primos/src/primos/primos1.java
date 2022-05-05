ca4e993431a95a6cd7bd01a617788bc51400e66b
GustavoAntunes-dev <gustavo.eng8@hotmail.com>
2022-05-05 12:43:30 +0000

    primosDone

--- Primos/primos/src/primos/primos1.java
+++ Primos/primos/src/primos/primos1.java
@@ -1,10 +1,32 @@
 package primos;
 
+import java.util.Scanner;
+
 public class primos1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		int n = sc.nextInt();
+		boolean isPrime = isPrime(n);
+		System.out.printf("number %d is prime %b", n, isPrime);
+
+		sc.close();
 	}
 
+	public static boolean isPrime(int n) {
+		int p = 2;
+		while (p < n && n != 2) {
+			boolean isDivisible = isDivisible(n, p);
+			if (isDivisible) {
+				return false;
+			}
+			p++;
+		}
+		return true;
+	}
+
+	public static boolean isDivisible(int dividend, int division) {
+		return (dividend % division) == 0;
+	}
 }
